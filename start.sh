2db1776ebb684ed9b1ee3e30ad425654aa57dcbe
gapolinario <gabrielbritoapolinario@gmail.com>
2018-03-08 21:10:04 -0300

    Cron might work one day

--- start.sh
+++ start.sh
@@ -9,3 +9,12 @@ read user
 printf "Date,Number\n" > $folder/watchRecord.csv
 
 printf "folder: $folder\nuser: $user\n" > config
+
+# make a bash executable folder and copy it to
+# cron.weekly, this might work
+
+# printf("#!/bin/bash\nmake -C $folder all")
+
+# or something like this,
+# in fact it's better to set up a different
+# make command, like make set_cron
